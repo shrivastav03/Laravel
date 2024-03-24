b2149da432f916b408617fce8aa5cc3b2ae11f30
shrivastav03 <125891191+shrivastav03@users.noreply.github.com>
2024-03-24 21:08:32 +0530

    Create form.blade.php

---
+++ form.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+    <form action="{{url('/')}}/register" method="post">
+    @csrf
+    <div class="container">
+        <h1 class="text-center">Registration</h1>
+        <div class="form-group">
+            <label for="">User Name</label>
+            <input type="text" name="name" id="" class="form-control" /> 
+        </div>
+
+        <div class="form-group">
+            <label for="">Email</label>
+            <input type="email" name="email" id="" class="form-control"/>
+        </div>
+
+        <div class="form-group">
+            <label for="">Password</label>
+            <input type="password" name="password" id="" class="form-control" placeholder="" aria-describedby="helpId"/>
+        </div>
+
+        <div class="form-group">
+            <label for="">Comfirm Password</label>
+            <input type="password" name="comfirm_password" id="" />
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+
+    </div>
+    </form>
+  </body>
+</html>
